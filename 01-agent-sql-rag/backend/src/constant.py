433f7b521426e56e6b9f258af3aef3aea240549b
fahmiaziz98 <fahmiazizfadhil09@gmail.com>
2024-10-29 20:33:23 +0700

    init

--- 01-agent-sql-rag/backend/src/constant.py
+++ 01-agent-sql-rag/backend/src/constant.py
@@ -3,3 +3,10 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY") 
+DB_PATH = os.getenv("DB_PATH")
+
+CONFIG = {
+    "configurable" : {
+        "thread_id": "1234"
+    }
+}
